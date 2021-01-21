7a7d9e7646a563f5954ad1b4ef47e2fb4ef295e5
jumperko <jumperko@gmail.com>
2021-01-21 21:01:18 +0100

    doplneny detail page

--- web.py
+++ web.py
@@ -15,5 +15,11 @@ def index():
     return render_template("index.html", stats_data=stats_data.items_stats)
 
 
+@app.route("/item/<item_id>")
+def item_detail(item_id):
+    data_for_web = data_collector.find_by_id(item_id)
+    return render_template("item_price_history.html", selected_idem_data=data_for_web)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
