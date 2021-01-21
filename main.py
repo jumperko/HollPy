89ebc355180e0ff21f3e1ec634e370b04ff596f3
jumperko <jumperko@gmail.com>
2021-01-21 10:11:45 +0100

    pocitanie priemeru z itemu

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ hollister = HollisterData()
 # hollister.export_data_to_db()
 lookup = hollister.find_by_id("39915320")
 for item in lookup:
-    print(f"{item[0]}")
+    print(f"iD:{item[0]}, Price: {item[3]}, date: {item[1]}")
+
+price_compare = [price[3] for price in lookup]
+print(price_compare)
+av_price = sum(price_compare) / len(price_compare)
+print(av_price)
